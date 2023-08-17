82d5e564c1a3fcdf50c1e378cb23e6d4f15d6121
Ron <32454051+ronksks@users.noreply.github.com>
2023-08-17 16:38:01 +0300

    added CoachForm.vue and CoachRegistration.vue logic to the form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,20 @@
 <template>
-  REGISTER
+  <section>
+    <base-card>
+
+    <h2>Register as a coach now!</h2>
+    <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+<script>
+import CoachForm from "@/components/coaches/CoachForm.vue";
+import BaseCard from "@/components/ui/BaseCard.vue";
+
+export default {
+  components: {
+    BaseCard,
+    CoachForm
+  }
+}
+</script>
